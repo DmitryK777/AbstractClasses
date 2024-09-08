2d2e5da93727d88214f3cda5f3aae1c8464ce538
DmitryK777 <kislyakovdmv@gmail.com>
2024-09-08 17:45:27 +0300

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Wolf wolf = new Wolf();
+			wolf.Sound();
+
+			Chihuahua chihuahua = new Chihuahua();
+			chihuahua.Sound();
+		}
+	}
+}
